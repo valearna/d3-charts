e4bfc405f97f9f3ec69eaab9080ef710cd6379fc
Valerio Arnaboldi <valearna@caltech.edu>
2021-05-25 18:29:13 -0700

    feat(swarmplot)!: added swarmplot

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,6 +1,6 @@
 import * as $ from 'jquery';
 import * as d3 from 'd3';
-import {Ridgeline, Dotplot} from "@wormbase/d3-charts";
+import {Ridgeline, Dotplot, Swarmplot} from "@wormbase/d3-charts";
 
 $(document).ready(() => {
     let dotplot = new Dotplot("#dotplot", 80, 25, 150, 100, 1200, 800, 0, 0.1, 0.01, 30, true, 20);
@@ -17,6 +17,9 @@ $(document).ready(() => {
         });
         threeColsData = threeColsData.slice(0, cellNames.length * 10)
         dotplot.draw(threeColsData);
-    })
+    });
+
+    let swarmplot = new Swarmplot("#dotplot", 80, 25, 150, 100, 1200, 800, 30);
+    swarmplot.draw([{x:-10, y:"lin-2", tooltip_html: "<div>Test</div>"}, {x:2, y:"lin-1", tooltip_html: "<div>Test</div>"}, {x:3, y:"lin-1", tooltip_html: "<div>Test</div>"}, {x:4, y:"lin-2", tooltip_html: "<div>Test</div>"}]);
 
 })
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Heatmap } from "./heatmap";
 export { default as Ridgeline } from "./ridgeline";
 export { default as Dotplot } from "./dotplot";
+export { default as Swarmplot } from "./swarmplot";
